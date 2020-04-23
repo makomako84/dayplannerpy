db3a64b4fd72623256f4475d2ab0f5ea6a26161f
Galimsky <Artur.Galimsky@primetals.com>
2020-04-23 20:08:15 +0500

    Some functional programming

--- API/Objects/Task.py
+++ API/Objects/Task.py
@@ -2,15 +2,12 @@ from API.Utils.timeutils import  deserializeDate, serializeDate
 
 
 class Task:
-    name = None
-    date = None
-    description = None
-    done = False
 
-    def __init__(self, name,done, date):
+    def __init__(self, name,done, date, *, description=None):
         self.name = name
         self.done = done
         self.date = date
+        self.description = description
     @staticmethod
     def decodeJSON(jsondata):
         newTask = Task(name=jsondata["name"], done=jsondata["done"], date=deserializeDate(jsondata["date"]))
@@ -25,6 +22,10 @@ class Task:
             "done": self.done,
             "date": serializeDate(self.date)
         }
+
+    def __ne__(self, other):
+        assert isinstance(other, Task), "other is not instance of Task"
+        return self.name != other.name
 
     def __str__(self):
         return f"{self.name}, {self.done}, {self.description}, {self.date}"
--- dayplanner.py
+++ dayplanner.py
@@ -5,26 +5,31 @@ from datetime import  datetime
 from API.Objects.Task import Task
 import  os
 
-print(__file__)
+# private
 DATA_FILE_NAME = os.path.join(os.path.dirname(__file__), 'data.json')
 
 
 
-
 class DayPlanner:
-    data = {
+
+
+    __data = {
         "Tasks": [],
         "Phones": []
     }
 
     def __init__(self):
         # self.test_create_tasks()
-        self.initObjectData()
+        self.__initObjectData()
         self.shell_main_menu()
-
         # self.updateJSON()
-
         # self.shell_main_menu()
+
+    def __initObjectData(self):
+        nonobjectdata = self.readJSON()
+        for task in nonobjectdata["Tasks"]:
+            self.__data["Tasks"].append(Task.decodeJSON(task))
+
     def shell_main_menu(self):
         if(len(sys.argv) <2): return
 
@@ -36,61 +41,83 @@ class DayPlanner:
             if secondarg == 'out':
                 self.out_tasks()
             elif secondarg == 'add':
-                self.shell_create_task()
+                self.shell_add_task()
+            elif secondarg == 'find':
+                self.shell_find_task()
+            elif secondarg == 'del':
+                self.shell_del_task()
         elif firstarg == '-book':
             if (len(sys.argv) < 3): return
             secondarg = sys.argv[2]
             if secondarg == 'out':
                 self.out_phones()
 
-    def shell_create_task(self):
+    def shell_add_task(self):
         ui = {}
         ui["name"] = input("Enter task name: ")
         ui["date"] = input("enter date in form: YYYY-M-D H:M ")
         ui["done"] = True if input("Is task done? y/n ") else False
         self.add_new_task(ui)
 
+    def shell_find_task(self):
+        ui={}
+        ui["name"] = input("Enter part of task name: ")
+
+        searchfunc = lambda x: x.name.find(ui["name"])!=-1
+        res = list(filter(searchfunc, self.__data["Tasks"]))
+        [print(x) for x in res]
+        return  res
+
+    def shell_del_task(self):
+        res = self.shell_find_task()
+        if len(res) > 1:
+            print("Create more Accureate request")
+        else:
+            delaquired = True if input("Delete? (y)") == "y" else False
+            if(delaquired):
+                self.__data["Tasks"]  = list(filter((lambda task: res[0]!=task),self.__data["Tasks"]))
+                self.updateJSON()
+
+
+    # tasks functions
     def out_tasks(self):
-        [print(task.__str__()+"\n") for task in self.data["Tasks"]]
+        [print(task.__str__()+"\n") for task in self.__data["Tasks"]]
+    def add_new_task(self,dict):
+        self.__data["Tasks"].append(Task.decodeJSON(dict))
+        self.updateJSON()
+
     def out_phones(self):
         print(*self.phones, sep='\n')
 
+
+    # work with JSON
     def readJSON(self):
         with open(DATA_FILE_NAME, "r") as read_file:
             return json.load(read_file)
-
     def updateJSON(self):
         jsondata = {
-            "Tasks" : [task.encodeJSON() for task in self.data["Tasks"]],
+            "Tasks" : [task.encodeJSON() for task in self.__data["Tasks"]],
             "Phones" : []
         }
         with open(DATA_FILE_NAME, "w") as write_file:
             json.dump(jsondata, write_file, indent=4)
 
-    def initObjectData(self):
-        nonobjectdata = self.readJSON()
-        for task in nonobjectdata["Tasks"]:
-            self.data["Tasks"].append(Task.decodeJSON(task))
-        # pprint.pprint(self.data)
-        # [print(task) for task in self.data["Tasks"]]
 
-    def add_new_task(self,dict):
-        self.data["Tasks"].append(Task.decodeJSON(dict))
+
+    def test_create_new_task(self, name, done, date):
+        self.__data["Tasks"].append(Task(name, done, date))
         self.updateJSON()
 
-    def create_new_task(self, name, done, date):
-        self.data["Tasks"].append(Task(name, done, date))
-        self.updateJSON()
-
-    def craete_new_phone(self):
+    def test_craete_new_phone(self):
         pass
 
     def test_create_tasks(self):
         t1 = Task("Buy clothes", True, datetime.now())
         t2 = Task("Update pip packages", False, datetime.now())
-        self.data["Tasks"].append(t1)
-        self.data["Tasks"].append(t2)
+        self.__data["Tasks"].append(t1)
+        self.__data["Tasks"].append(t2)
 
 
 dayPlanner = DayPlanner()
+# dayPlanner.shell_del_task()
 # print(sys.argv[1])
